d56411abd0c015f9cc788a2a2db10e60700a5e2a
Maxime Florile <florile.maxime@gmail.com>
2015-11-25 13:48:53 +0100

    modif step5

--- src/comics/components/comics/ComicsItem.jsx
+++ src/comics/components/comics/ComicsItem.jsx
@@ -4,12 +4,40 @@ import React, { Component } from 'react';
 
 export default class ComicsItem extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      over: false
+    }
+  }
+
+  componentWillMount(){
+    console.log("Will", this.props);
+  }
+
+  componentDidMount(){
+    console.log("Did", this.props);
+  }
+
+  //Using state to change component behavior
+  toggleState(){
+    this.setState({
+      over : !this.state.over
+    });
+  }
+
   render() {
+
+    let { className, imageUrl } = this.props;
+    let overClass = this.state.over ? 'overlay overlay--over' : 'overlay';
+
     return (
-      <div className="comicsItem">
+      <div className="comicsItem"
+           onMouseOver={this.toggleState.bind(this)}
+           onMouseOut={this.toggleState.bind(this)}>
         <a href="#">
-           <img src="http://lorempixel.com/100/200/"/>
-           <div>
+           <img src="http://lorempixel.com/400/200"/>
+           <div className="overlay">
              <p>SERIE</p>
              <p>NAME</p>
              <p>PAGE COUNT</p>
